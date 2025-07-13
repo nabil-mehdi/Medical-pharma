829020988cb9b63664cc76860fd467a75546e054
John Doe <john.doe@example.com>
2025-07-13 19:30:29 +0100

    Force Vite assets to use ASSET_URL (HTTPS)

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
+const assetUrl = process.env.ASSET_URL ? process.env.ASSET_URL + '/build/' : '/build/';
+
 export default defineConfig({
+    base: assetUrl,
     plugins: [
         laravel({
             input: 'resources/js/app.js',
